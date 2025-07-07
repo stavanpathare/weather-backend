a05e1ff11d2ea9dd4752ae3e05e5f6382a2be396
Stavan Pathare <stavanpathare@gmail.com>
2025-07-07 22:35:43 +0530

    Updated code with new features / fixes

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.get('/weather', async (req, res) => {
     }
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
 });
+
 
